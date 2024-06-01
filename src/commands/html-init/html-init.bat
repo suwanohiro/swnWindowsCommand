ec3a578ba89174403975be4fa8c190edc02dfd61
suwanohiro <supermariomaker1110@gmail.com>
2024-06-01 10:36:47 +0900

    TypeScriptの初期化処理を追加

--- src/commands/html-init/html-init.bat
+++ src/commands/html-init/html-init.bat
@@ -7,11 +7,15 @@ cd D:\test
 
 echo "HTMLプロジェクトを作成します"
 
-
 call npm init --y
+
+:: Nodeのインストール処理
 call npm install typescript
 call npm install sass
 call npm install rimraf
+
+:: TypeScriptの初期化処理
+call tsc --init
 
 mkdir src
 
