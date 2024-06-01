2a40dfda2ea10d6be86c4b3b6ad66f4887ef5b6a
suwanohiro <supermariomaker1110@gmail.com>
2024-06-01 10:30:36 +0900

    Node側のインストール処理を作成

--- src/commands/html-init/html-init.bat
+++ src/commands/html-init/html-init.bat
@@ -1,3 +1,18 @@
 @echo off
 
+:: HTMLプロジェクトを作成するコマンド
+
+:: TODO: テスト用にディレクトリを移動しているだけなので後で消す
+cd D:\test
+
+echo "HTMLプロジェクトを作成します"
+
+
+call npm init --y
+call npm install typescript
+call npm install sass
+call npm install rimraf
+
+mkdir src
+
 echo This is html-init
