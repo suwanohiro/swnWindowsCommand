570d449ff11af4f808d96ebddb79e8fb9074f045
suwanohiro <supermariomaker1110@gmail.com>
2024-06-01 12:05:58 +0900

    空行削除

--- src/commands/html-init/Module/create_Application_ts.bat
+++ src/commands/html-init/Module/create_Application_ts.bat
@@ -5,6 +5,5 @@ echo export default class Application ^{> %OUTPUT_FILE%
 echo     constructor^(^) ^{^}>> %OUTPUT_FILE%
 echo.>> %OUTPUT_FILE%
 echo     public Start^(^): void ^{>> %OUTPUT_FILE%
-echo        >> %OUTPUT_FILE%
 echo     ^}>> %OUTPUT_FILE%
 echo ^}>> %OUTPUT_FILE%
