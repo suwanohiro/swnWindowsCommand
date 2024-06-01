04e59b3eebc17c94984ccd16cb9844fd993e471e
suwanohiro <supermariomaker1110@gmail.com>
2024-06-01 10:30:12 +0900

    子ファイルで正常に各種コマンドが実行できるように修正

--- src/swn.bat
+++ src/swn.bat
@@ -3,8 +3,14 @@
 set command=%1
 shift
 
+:: カレントディレクトリを保存
+set saved_dir=%cd%
+
 if "%command%" == "html-init" (
     call commands\html-init\html-init.bat
 )
+
+:: カレントディレクトリを復元
+cd /d %saved_dir%
 
 echo "end tasks"
